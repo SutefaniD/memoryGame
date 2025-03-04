4094a5f2d554f776f3f9f72d6f933e84bcabc500
Stephanie DURANTE <110696367+SutefaniD@users.noreply.github.com>
2025-03-04 09:56:02 +0100

    squelette html + css sans bootstrap formulaire

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Check the subscription form */
+
+
+function checkCriteria() {
+    
+    
+    document.getElementById("userName").addEventListener("input", function() {
+        let userInput = this.value;
+        if (userInput.length < 3) {
+            
+        }
+    })
+    element.addEventListener("keydown", (e) => {
+        if ()
+    })
+    console.log();
+    while (element.length < 3)
+
+
+    //img.src = "assets/check.svg";
+
+}
+
+function init() {
+
+
+    document.getElementById('submit-btn').addEventListener('click', checkCriteria);
+    // document.getElementById('mdp').addEventListener('input', checkPassword);
+
+}
+
+window.onload = init;
+
